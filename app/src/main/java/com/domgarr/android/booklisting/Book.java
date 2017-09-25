27e4e9613672849de89356ae8c8674ac6db974fa
Domenic Garreffa <garreffd@uwindsor.ca>
2017-09-24 20:59:05 -0400

    Added Book Class

---
+++ app/src/main/java/com/domgarr/android/booklisting/Book.java
@@ -0,0 +1,102 @@
+package com.domgarr.android.booklisting;
+
+/**
+ * Created by Domenic on 2017-09-24.
+ */
+
+public class Book {
+    private final String mTitle;
+    private final String mAuthor;
+    private final String mPublishedDate;
+    private final String mDescription;
+    private final String mImageLink;
+    private final Double mAverageRating;
+    private final String mPreviewLink;
+
+    //This class implements a builder pattern. It is useful when the constructor
+    //requires many parameters. Extremely useful when there are optional parameters.
+    //Although my implementation of Book will require everything, the one pros of using
+    //Builder pattern is that it makes code more readable.
+    public static class BookBuilder {
+        //Required Parameters
+        private String mTitle;
+        private String mAuthor;
+        private String mImageLink;
+        private String mDescription;
+
+        //Optional Parameters - Initialized to default values
+        private  String mPublishedDate = null;
+        private  Double mAverageRating = null;
+        private  String mPreviewLink = null;
+
+        public BookBuilder publishedDate(String publishedDate){
+            mPublishedDate = publishedDate;
+            return this;
+        }
+
+        public BookBuilder mAverageRating(Double AverageRating){
+            mAverageRating = mAverageRating;
+            return this;
+        }
+
+        public BookBuilder mPreviewLink(String previewLink){
+            mPreviewLink = previewLink;
+            return this;
+        }
+
+        public Book build(){
+            return new Book(this);
+        }
+    }
+
+    private Book (BookBuilder bookBuilder){
+        mTitle = bookBuilder.mTitle;
+        mAuthor = bookBuilder.mAuthor;
+        mPublishedDate = bookBuilder.mPublishedDate;
+        mDescription = bookBuilder.mDescription;
+        mImageLink = bookBuilder.mImageLink;
+        mAverageRating = bookBuilder.mAverageRating;
+        mPreviewLink = bookBuilder.mPreviewLink;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    public String getPublishedDate() {
+        return mPublishedDate;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public String getImageLink() {
+        return mImageLink;
+    }
+
+    public Double getAverageRating() {
+        return mAverageRating;
+    }
+
+    public String getPreviewLink() {
+        return mPreviewLink;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "mTitle='" + mTitle + '\'' +
+                ", mAuthor='" + mAuthor + '\'' +
+                ", mPublishedDate='" + mPublishedDate + '\'' +
+                ", mDescription='" + mDescription + '\'' +
+                ", mImageLink='" + mImageLink + '\'' +
+                ", mAverageRating=" + mAverageRating +
+                ", mPreviewLink='" + mPreviewLink + '\'' +
+                '}';
+    }
+}
